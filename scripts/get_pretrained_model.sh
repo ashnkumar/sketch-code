8e69ff3beb5cea4ba81f465bbf168f79f2aba762
Ashwin Kumar <ash.nkumar@gmail.com>
2018-02-07 17:17:23 -0800

    Typo

--- scripts/get_pretrained_model.sh
+++ scripts/get_pretrained_model.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 mkdir ../bin
 wget http://sketch-code.s3.amazonaws.com/model_json_weights/model_json.json -O ../bin/model_json.json
-wget http://sketch-code.s3.amazonaws.com/model_json_weights/weights.h5 -I ../bin/weights.h5
+wget http://sketch-code.s3.amazonaws.com/model_json_weights/weights.h5 -O ../bin/weights.h5
 
