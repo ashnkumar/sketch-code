09653f60b0c03d17eb2b623aec634db3e3be44c9
Ashwin Kumar <ash.nkumar@gmail.com>
2018-03-19 17:19:16 -0700

    Modified get_model file

--- scripts/get_pretrained_model.sh
+++ scripts/get_pretrained_model.sh
@@ -3,3 +3,4 @@ mkdir ../bin
 wget http://sketch-code.s3.amazonaws.com/model_json_weights/model_json.json -O ../bin/model_json.json
 wget http://sketch-code.s3.amazonaws.com/model_json_weights/weights.h5 -O ../bin/weights.h5
 
+
